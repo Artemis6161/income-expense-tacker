1947b0a851ae7a7e63e278ab03817d55b4d737f9
sangeetha <Artemis6161@github.com>
2024-11-11 21:08:40 +0530

    added options in index.js

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json({ extended: false }));
 
+app.options('*', cors());
 
 const corsOptions = {
   origin: 'https://income-expense-tacker-frontend.onrender.com', // Update to frontend URL
